d409ca99d82c8c8bb45ce41d563d7239a7f9139a
ankitagupta2 <ankitagupta2@ard01.mioct.ext.sopra>
2019-12-30 16:16:05 +0530

    first commit

---
+++ src/main/java/com/login/Login.java
@@ -0,0 +1,37 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Login() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		
+		if(firstName.equalsIgnoreCase("ankit") && lastName.equalsIgnoreCase("gupta")) {
+			response.sendRedirect("home.jsp");
+		}
+		else {
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Login</title>
+</head>
+<body>
+<form action="Login" method="post">
+First Name: <input type="text" name="firstname" /><br/>
+Last Name: <input type="text" name="lastname" /><br/>
+<input type="submit" value="Submit" />
+
+</form>
+</body>
+</html>
