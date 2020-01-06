b71db4fc623da58760c2f6b1c8573b3b22fb777f
ankitagupta2 <ankitagupta2@ard01.mioct.ext.sopra>
2020-01-06 16:26:04 +0530

    changed user from root to ankitgupta

--- src/main/java/com/database/DBInfo.java
+++ src/main/java/com/database/DBInfo.java
@@ -1,12 +1,9 @@
 package com.database;
 
-
-
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.mysql.cj.jdbc.ConnectionImpl;
-
 
 public class DBInfo {
 
@@ -16,14 +13,26 @@ public class DBInfo {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = (ConnectionImpl) DriverManager.getConnection("jdbc:mysql://localhost:3306/user", "root", "Mysql123@");
-			
+
+			// String host = InetAddress.getLocalHost().getHostAddress();
+			/*
+			 * String host = System.getenv("hostname"); System.out.println("host name is "+
+			 * host); String port = System.getenv("MYSQL_SERVICE_PORT");
+			 * System.out.println("host name is "+ port);
+			 */
+
+			con = (ConnectionImpl) DriverManager.getConnection("jdbc:mysql://localhost:3306/user", "ankitgupta",
+					"ankit123@");
+
+			// con = (ConnectionImpl)
+			// DriverManager.getConnection("jdbc:mysql://"+host+":3306/user", "ankitgupta",
+			// "ankit123@");
 
 		} catch (ClassNotFoundException notFoundException) {
 			System.out.println(notFoundException.getMessage());
 
-		} catch(SQLException exception) {
-			System.out.println("Got an exception = "+exception.getMessage());
+		} catch (SQLException exception) {
+			System.out.println("Got an exception = " + exception.getMessage());
 		}
 
 		return con;
