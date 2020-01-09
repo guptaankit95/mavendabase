e5dafe9e9f463b9d17086da43c9ed3b991500332
ankitagupta2 <ankitagupta2@ard01.mioct.ext.sopra>
2020-01-09 11:02:18 +0530

    checking the access environment access

--- src/main/java/com/database/DBInfo.java
+++ src/main/java/com/database/DBInfo.java
@@ -16,33 +16,43 @@ public class DBInfo {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
-			String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+			//System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+			String host =InetAddress.getLocalHost().getHostName(); 
+					
 			String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
 
-			con = (ConnectionImpl) DriverManager.getConnection("jdbc:mysql://localhost:3306/user", "ankitgupta",
-					"ankit123@");
+			
+			
 
-			// con = (ConnectionImpl) DriverManager.getConnection("jdbc:mysql://" + host +
-			// ":" + port + "/user",
-			// "ankitgupta", "ankit123@");
+			// con = (ConnectionImpl)
+			// DriverManager.getConnection("jdbc:mysql://localhost:3306/user", "ankitgupta",
+			// "ankit123@");
+
+			con = (ConnectionImpl) DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/user",
+					"ankitgupta", "ankit123@");
 
 			// DriverManager.getConnection("jdbc:mysql://"+host+":3306/user", "ankitgupta",
 			// "ankit123@");
 
-			System.out.println("system hostname is " + InetAddress.getLocalHost().getHostName().toString());
+			System.out.println("system hostname is " + InetAddress.getLocalHost().getHostName());
+			
 
 			System.out.println("jdbc:mysql://" + host + ":" + port);
 			System.out.println("Connection Established Successfull and the DATABASE NAME IS:"
 					+ con.getMetaData().getDatabaseProductName());
 
-			System.out.println("host name is " + System.getenv("OPENSHIFT_MYSQL_DB_HOST"));
-			System.out.println("port name is " + System.getenv("OPENSHIFT_MYSQL_DB_PORT"));
+			System.out.println("host name is " + System.getenv("LOCALHOST_SERVICE_HOST"));
+			System.out.println("port name is " + System.getenv("LOCALHOST_SERVICE_PORT"));
 			System.out.println("after executing host name is " + con.getHostPortPair());
 
 		} catch (ClassNotFoundException notFoundException) {
+			System.out.println("host name is " + System.getenv("LOCALHOST_SERVICE_HOST"));
+			System.out.println("port name is " + System.getenv("LOCALHOST_SERVICE_PORT"));
 			System.out.println(notFoundException.getMessage());
 
 		} catch (SQLException exception) {
+			System.out.println("host name is " + System.getenv("LOCALHOST_SERVICE_HOST"));
+			System.out.println("port name is " + System.getenv("LOCALHOST_SERVICE_PORT"));
 			System.out.println("Got an exception = " + exception.getMessage());
 		} catch (UnknownHostException e) {
 
